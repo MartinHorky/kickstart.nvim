dde5ab53839d8884d7a2198a2b70c48ac2445aac
Martin Horky <martinhorky121@gmail.com>
2024-06-24 12:14:36 +0200

    local config

--- init.lua
+++ init.lua
@@ -154,6 +154,9 @@ vim.opt.cursorline = true
 -- Minimal number of screen lines to keep above and below the cursor.
 vim.opt.scrolloff = 10
 
+vim.opt.exrc = true
+vim.opt.secure = true
+
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
 
