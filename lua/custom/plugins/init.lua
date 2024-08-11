333e374c153b07f54c38fb5faa16ab8e1ca69c1a
Martin Horky <martinhorky121@gmail.com>
2024-08-11 11:33:58 +0200

    openscad plugin

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -56,4 +56,13 @@ return {
       { '<c-\\>', '<cmd><C-U>TmuxNavigatePrevious<cr>' },
     },
   },
+  {
+    'salkin-mada/openscad.nvim',
+    config = function()
+      require 'openscad'
+      -- load snippets, note requires
+      vim.g.openscad_load_snippets = true
+    end,
+    requires = 'L3MON4D3/LuaSnip',
+  },
 }
